3de3a6c5506b3bd160ba7046901f17bfeae65b3e
Sam Gunaratne <sgunaratne@vmware.com>
2022-09-30 18:43:26 +0000

    updated example

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 using Newtonsoft.Json;
+using StackExchange.Redis;
 
 namespace Sample.Controllers;
 
@@ -22,9 +23,61 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
+
     [HttpGet]
     public WeatherForecast Get()
     {
+        //Setup redis client
+        var redisConn = RedisClient();
+        IDatabase db = redisConn.GetDatabase();
+
+        //Setup rmq client
+        IModel channel = RabbitmqClient();
+        channel.QueueDeclare(queue: "weather",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+        //Generate Weather Data
+        var weather = new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(0),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            };
+
+        //Send weather data to RMQ
+        var jsonWeather = JsonConvert.SerializeObject(weather);
+    
+        var body = Encoding.UTF8.GetBytes(jsonWeather);
+
+        channel.BasicPublish(exchange: "",
+                            routingKey: "weather",
+                            basicProperties: null,
+                            body: body);
+
+        Console.WriteLine(" [->] Sent {0}",jsonWeather);
+
+
+        //Receive weather data from RMQ
+        var response = channel.BasicGet(queue: "weather", autoAck: true);
+
+        var recbody = response.Body.ToArray();
+        var recmessage = Encoding.UTF8.GetString(recbody);
+        Console.WriteLine(" [<-] Received {0}", recmessage);
+
+
+        //Store weather data in Redis
+        db.StringSet("weather", recmessage);
+        Console.WriteLine(" [!] Stored in Redis {0}", recmessage);
+
+        //Return in JSON
+        var recweather = JsonConvert.DeserializeObject < WeatherForecast > (recmessage);
+        return recweather;
+    }
+
+    private IModel RabbitmqClient() {
         //Parse RMQ configuration
         string host = Environment.GetEnvironmentVariable("RMQ_HOST");
         string port = Environment.GetEnvironmentVariable("RMQ_PORT");
@@ -40,43 +93,28 @@ public class WeatherForecastController : ControllerBase
 
         IConnection conn = factory.CreateConnection();
         
-        IModel channel = conn.CreateModel();
-        channel.QueueDeclare(queue: "temperature",
-                                durable: false,
-                                exclusive: false,
-                                autoDelete: false,
-                                arguments: null);
-
-        //Generate Weather Data
-        var weather = new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(0),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            };
-
-        //Send weather data
-        var jsonWeather = JsonConvert.SerializeObject(weather);
-    
-        var body = Encoding.UTF8.GetBytes(jsonWeather);
-
-        channel.BasicPublish(exchange: "",
-                            routingKey: "temperature",
-                            basicProperties: null,
-                            body: body);
-
-        Console.WriteLine(" [->] Sent {0}",jsonWeather);
+        return conn.CreateModel();
+    }
 
 
-        //Receive weather data
-        var response = channel.BasicGet(queue: "temperature", autoAck: true);
+    private ConnectionMultiplexer RedisClient() {
+        //Parse Redis configuration
+        string host = Environment.GetEnvironmentVariable("REDIS_HOST");
+        string port = Environment.GetEnvironmentVariable("REDIS_PORT");
+        string password = Environment.GetEnvironmentVariable("REDIS_PASSWORD");
 
-        var recbody = response.Body.ToArray();
-        var recmessage = Encoding.UTF8.GetString(recbody);
-        Console.WriteLine(" [<-] Received {0}", recmessage);
+        ConfigurationOptions config = new ConfigurationOptions
+        {
+            AbortOnConnectFail = true,
+            Ssl = false, //WARNING UNSAFE
+            ConnectRetry = 3,
+            ConnectTimeout = 5000,
+            SyncTimeout = 5000,
+            DefaultDatabase = 0,
+            EndPoints = { { host,  Int32.Parse(port) } },
+            Password = password
+        };
 
-        var recweather = JsonConvert.DeserializeObject < WeatherForecast > (recmessage);
-
-        return recweather;
+        return ConnectionMultiplexer.Connect(config);
     }
 }
